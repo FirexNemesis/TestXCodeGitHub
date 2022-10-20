8019e3495db6414b028c3de15a107e303004d21a
Riccardo Gonzato <gonzatoriccardo17@gmail.com>
2022-10-20 18:33:37 +0200

    Initial Commit

---
+++ TestGitHub/TestGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubApp.swift
+//  TestGitHub
+//
+//  Created by Riccardo Gonzato on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
